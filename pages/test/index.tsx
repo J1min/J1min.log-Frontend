b0716ecc877e1d3acf199ba79e34d43d0db3d9e1
J1min <jmhong101506@gmail.com>
2022-10-12 00:14:24 +0900

    FIX :: 이슈템플릿 수정

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -5,12 +5,10 @@ import { getUserInfo } from "../../api/response/getTest";
 const Test: NextPage = () => {
   const [data, setData] = React.useState<string>("");
 
-  console.log(
-    getUserInfo().then((response) => {
-      console.log(response);
-      setData(response.이즈나);
-    })
-  );
+  getUserInfo().then((response) => {
+    setData(response.이즈나);
+  });
+
   return <main>{data}</main>;
 };
 
