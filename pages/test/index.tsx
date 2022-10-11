d6ad70f0af820b33a4bd7fedbcce030573b5cc4d
J1min <jmhong101506@gmail.com>
2022-10-11 13:17:49 +0900

    ADD :: instance으로 Axios 통신하는 테스트 추가

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import type { NextPage } from "next";
-import { result } from "../../api/getTest";
-
+import { getUserInfo } from "../../api/response/getTest";
 
 const Test: NextPage = () => {
-  console.log(result);
+  const [data, setData] = React.useState<string>("");
 
-  return <main></main>;
+  console.log(
+    getUserInfo().then((response) => {
+      console.log(response);
+      setData(response.이즈나);
+    })
+  );
+  return <main>{data}</main>;
 };
 
 export default Test;
