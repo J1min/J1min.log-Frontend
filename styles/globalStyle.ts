93177598eedfbc006a5ec942b2a5490fe5744db4
J1min <aoiharu41@gmail.com>
2022-11-23 08:00:01 +0900

    FIX :: 글로벌 스타일 수정

--- styles/globalStyle.ts
+++ styles/globalStyle.ts
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle` 
 html {
   box-sizing: border-box;
-  font-size: 20px;
+  font-size: 130%;
   min-width: 320px;
   line-height: 1.15;
   -webkit-text-size-adjust: 100%;
@@ -24,65 +24,10 @@ hr {
   overflow: visible;
 }
 
-pre {
-  font-family: monospace, monospace;
-  font-size: 1em;
-}
-
-a {
-  cursor: pointer;
-  text-decoration: none;
-  background-color: transparent;
-}
-
-abbr[title] {
-  border-bottom: none;
-  text-decoration: underline;
-  text-decoration: underline dotted;
-}
-
-b,
-strong {
-  font-weight: bolder;
-}
-
-code,
-kbd,
-samp {
-  font-family: monospace, monospace;
-  font-size: 1em;
-}
-
-small {
-  font-size: 80%;
-}
-
-sub,
-sup {
-  font-size: 75%;
-  line-height: 0;
-  position: relative;
-  vertical-align: baseline;
-}
-
-sub {
-  bottom: -0.25em;
-}
-
-sup {
-  top: -0.5em;
-}
-
-img {
-  border-style: none;
-}
-
-button,
 input,
 optgroup,
 select,
 textarea {
-  font-family: inherit;
   font-size: 100%;
   line-height: 1.15;
   margin: 0;
